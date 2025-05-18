90f301b982ab85832e803942b3af5176e9d7c414
黃凱揚 <ipad@mail2.smes.tyc.edu.tw>
2025-05-18 03:51:38 +0000

    請協助確認，當分享此網站的連結到社群平台時，網頁超連結下方的預覽卡片是否能正確出現此網站的大標題以及相關使用資訊細節

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,9 +13,41 @@ const geistMono = Geist_Mono({
   subsets: ['latin'],
 });
 
+const siteTitle = 'PIRLS 閱讀素養題組生成站';
+const siteDescription = '上傳圖片，APP 為您分析內容並設計PIRLS四層次選擇題。支援圖片貼上、PDF及Excel匯出。';
+// 建議在 public/images/ 建立一張名為 social-preview.png 的圖片 (推薦尺寸 1200x630)
+const socialPreviewImageUrl = '/images/social-preview.png'; 
+
 export const metadata: Metadata = {
-  title: 'PIRLS 閱讀素養題組生成站',
-  description: '上傳圖片，生成PIRLS閱讀素養題組',
+  title: siteTitle,
+  description: siteDescription,
+  openGraph: {
+    title: siteTitle,
+    description: siteDescription,
+    type: 'website',
+    // url: '您的網站部署後的完整URL', // 可選，通常平台會自動抓取
+    images: [
+      {
+        url: socialPreviewImageUrl,
+        width: 1200,
+        height: 630,
+        alt: siteTitle,
+      },
+    ],
+    siteName: siteTitle,
+    locale: 'zh_TW',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: siteTitle,
+    description: siteDescription,
+    images: [socialPreviewImageUrl],
+    // site: '@您的Twitter帳號', // 可選
+    // creator: '@您的Twitter帳號', // 可選
+  },
+  // 為了更好的搜尋引擎索引和可訪問性，可以加入其他標籤
+  // keywords: ['PIRLS', '閱讀素養', '題目生成', '教育科技', 'AI輔助教學'], // 可選
+  // authors: [{ name: '桃園市石門國小資訊組 阿凱老師', url: 'https://www.smes.tyc.edu.tw/' }], // 可選
 };
 
 export default function RootLayout({
